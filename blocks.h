dc1222d1e5772a7ceda6c005539c08bd0e7081e9
user <user@localhost>
2022-10-13 17:36:51 -0300

    updated blocks.h and gitignore

--- blocks.h
+++ blocks.h
@@ -2,12 +2,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", 		"sbtime",			1,			0},
-	{"",		"sbnwif enp1s0",		1,			14},
-	{"",		"sbnwif -w wlp2s0",		1,			15},
-	{"",		"sbaudio sink",			1,			10},
-	{"",		"sbaudio source",		1,			11},
-	{"", 		"sbmemory",			5,			12},
+	{"",		"sbnwif enp1s0",		1,			11},
+	{"",		"sbnwif -w wlp2s0",		1,			12},
+	{"", 		"sbmemory",			5,			13},
 	{"",		"sbnetwork",			1,			0},
+	{"",		"sbaudio sink",			1,			14},
+	{"",		"sbaudio source",		1,			15},
+	{"",		"sbpower",			0,			16},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
